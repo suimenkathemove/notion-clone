61573b4f21b3b993f22965d5f6c0e311cbaa9670
emgniddikur <yoshiharu2580@gmail.com>
2022-11-05 23:00:04 +0900

    add ports to docker-compose app

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,7 @@ use std::net::SocketAddr;
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
