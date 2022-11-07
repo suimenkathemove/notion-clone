db10f9f0b9b903ce88d4f3e12b3e77e25878ea44
emgniddikur <yoshiharu2580@gmail.com>
2022-11-08 05:14:28 +0900

    backend: add tracing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,20 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "backend=trace".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new().route("/", get(root));
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
