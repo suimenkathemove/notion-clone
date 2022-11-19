503e813eac3c74416126ff343f42a0258626bde7
emgniddikur <yoshiharu2580@gmail.com>
2022-11-19 16:20:34 +0900

    add new-window to dev.sh

--- dev.sh
+++ dev.sh
@@ -10,4 +10,12 @@ DEV_DIR="${HOME}/development/emgniddikur"
 
 cd "$DEV_DIR/$PROJECT_NAME"
 
-tmux-start "$PROJECT_NAME"
+if tmux has-session -t "$PROJECT_NAME"; then
+  tmux attach-session -t "$PROJECT_NAME"
+else
+  tmux new-session -s "$PROJECT_NAME" -d -n root
+  tmux new-window -n backend -c "./backend" 'make up'
+  tmux new-window -n frontend -c "./frontend" 'make up'
+  tmux select-window -t root
+  tmux attach-session -t "$PROJECT_NAME"
+fi
