a7db6d2d359381355e4dffd14dfa3cb1bfb164b6
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-10 08:51:44 +0900

    backend: add workspaces, channels, threads, messages table

--- backend/migrations/20221108225920_setup.sql
+++ backend/migrations/20221108225920_setup.sql
@@ -3,3 +3,19 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE accounts (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 );
+
+CREATE TABLE workspaces (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+);
+
+CREATE TABLE channels (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+);
+
+CREATE TABLE threads (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+);
+
+CREATE TABLE messages (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+);
