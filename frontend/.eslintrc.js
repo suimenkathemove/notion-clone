9d569c4331334feb45ea0a51aed42a55c6af2934
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-26 07:17:25 +0900

    frontend: add eslint, prettier

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [
+    // "@suimenkathemove/frontend-eslint-config",
+    "./node_modules/@suimenkathemove/frontend-eslint-config",
+  ],
+};
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+
 import { client } from "../graphql";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
