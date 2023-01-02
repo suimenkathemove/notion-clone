734a62b9ecab5387b312c7b3e3ee0f8d7c233ae8
suimenkathemove <yoshiharu2580@gmail.com>
2023-01-02 10:42:39 +0900

    infrastructure: add s3 to HelloWorldStack

--- infrastructure/cdk/hello-world/lib/hello-world-stack.ts
+++ infrastructure/cdk/hello-world/lib/hello-world-stack.ts
@@ -1,16 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class HelloWorldStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloWorldQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.aws_s3.Bucket(this, "MyFirstBucket", { versioned: true });
   }
 }
