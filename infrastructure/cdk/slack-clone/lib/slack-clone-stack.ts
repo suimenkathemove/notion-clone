54e5b27bcd0a8b18b52cfbb87858394a10f2173b
suimenkathemove <yoshiharu2580@gmail.com>
2023-01-03 20:17:07 +0900

    infrastructure: add VPC to cdk

--- infrastructure/cdk/slack-clone/lib/slack-clone-stack.ts
+++ infrastructure/cdk/slack-clone/lib/slack-clone-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import { Cidr } from "@/types";
 
 export class SlackCloneStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SlackCloneQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cidrBlock: Cidr = "10.0.0.0/16";
+    new cdk.aws_ec2.CfnVPC(this, "slackCloneVpc", {
+      cidrBlock,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+    });
   }
 }
