fc5368de33fc2c61545918204f043b0a01a7b9a8
suimenkathemove <yoshiharu2580@gmail.com>
2023-01-20 09:10:47 +0900

    infrastructure: add cidrBlocks

---
+++ infrastructure/cdk/slack-clone/constants/cidr-blocks.ts
@@ -0,0 +1,17 @@
+import { subnetIds, vpcId } from "./ids";
+
+import { Cidr } from "@/types";
+
+type SubnetIds = typeof subnetIds;
+type SubnetIdValues = SubnetIds[keyof SubnetIds];
+type SubnetIdValueValues = SubnetIdValues[keyof SubnetIdValues];
+
+export const cidrBlocks = {
+  slackCloneVpc: "10.0.0.0/16",
+  slackCloneSubnetPublicIngress1A: "10.0.0.0/24",
+  slackCloneSubnetPublicIngress1C: "10.0.0.1/24",
+  slackCloneSubnetPrivateApp1A: "10.0.0.8/24",
+  slackCloneSubnetPrivateApp1C: "10.0.0.9/24",
+  slackCloneSubnetPrivateDb1A: "10.0.0.16/24",
+  slackCloneSubnetPrivateDb1C: "10.0.0.17/24",
+} as const satisfies Record<typeof vpcId | SubnetIdValueValues, Cidr>;
