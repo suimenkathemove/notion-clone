ffb50fa4c9ffbe4b6258af93280e5aea842c0d07
suimenkathemove <yoshiharu2580@gmail.com>
2023-01-20 12:13:02 +0900

    add AvailabilityZoneType

--- infrastructure/cdk/slack-clone/constants/availability-zones.ts
+++ infrastructure/cdk/slack-clone/constants/availability-zones.ts
@@ -1,4 +1,6 @@
+export type AvailabilityZoneType = "a" | "c";
+
 export const availabilityZones = {
   a: "ap-northeast-1a",
   c: "ap-northeast-1c",
-} as const satisfies Record<string, string>;
+} as const satisfies Record<AvailabilityZoneType, string>;
