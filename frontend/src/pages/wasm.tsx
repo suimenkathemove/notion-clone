11d8fac9f0480eb7fdda9a0824a08ca57723eb41
suimenkathemove <yoshiharu2580@gmail.com>
2023-01-21 18:42:29 +0900

    setup wasm

---
+++ frontend/src/pages/wasm.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import { useEffect } from "react";
+
+const Wasm: NextPage = () => {
+  useEffect(() => {
+    (async () => {
+      const wasm = await import("wasm");
+      const { add } = await wasm.default();
+      // eslint-disable-next-line no-console
+      console.log(add(1, 2));
+    })();
+  }, []);
+
+  return <div />;
+};
+
+export default Wasm;
