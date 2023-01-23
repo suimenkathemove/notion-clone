6d03bda0c39805253893c77be353b4397539837f
suimenkathemove <yoshiharu2580@gmail.com>
2023-01-23 11:09:02 +0900

    backend: add FromStr to define_name macro

--- models/src/macros.rs
+++ models/src/macros.rs
@@ -8,6 +8,16 @@ macro_rules! define_id {
 
 macro_rules! define_name {
     ($name: ident) => {
+        use std::{str::FromStr, string::ParseError};
+
         pub struct $name(pub String);
+
+        impl FromStr for $name {
+            type Err = ParseError;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                Ok(Self(s.to_string()))
+            }
+        }
     };
 }
