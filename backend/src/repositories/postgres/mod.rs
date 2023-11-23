32552c7578e080a8b01e764c6f71acb06f12b41d
suimenkathemove <yoshiharu2580@gmail.com>
2023-11-23 18:40:42 +0900

    backend: separate DB by env variables

--- backend/src/repositories/postgres/mod.rs
+++ backend/src/repositories/postgres/mod.rs
@@ -8,8 +8,6 @@ mod utils;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 pub async fn create_pool() -> Pool<Postgres> {
-    dotenv::dotenv().ok();
-
     let database_url = std::env::var("DATABASE_URL").unwrap();
     PgPoolOptions::new().connect(&database_url).await.unwrap()
 }
