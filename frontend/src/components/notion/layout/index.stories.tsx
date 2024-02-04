31aff63c703f6661738996f4401810a301fbd8d6
suimenkathemove <yoshiharu2580@gmail.com>
2024-02-04 10:17:20 +0900

    fix

--- frontend/src/components/notion/layout/index.stories.tsx
+++ frontend/src/components/notion/layout/index.stories.tsx
@@ -12,13 +12,18 @@ const Sidebar = styled.aside`
   background-color: red;
 `;
 
-const Main = styled.main`
+const Header = styled.header`
   height: 100%;
   background-color: blue;
 `;
 
+const Main = styled.main`
+  height: 100%;
+  background-color: yellow;
+`;
+
 export const Default: StoryObj = {
   render: () => {
-    return <Layout sidebar={<Sidebar />} main={<Main />} />;
+    return <Layout sidebar={<Sidebar />} header={<Header />} main={<Main />} />;
   },
 };
