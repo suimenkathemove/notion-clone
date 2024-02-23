fd682dd2aa9be35de81a27ed28a41f87da3e0f4a
suimenkathemove <42399693+suimenkathemove@users.noreply.github.com>
2024-02-24 00:57:56 +0900

    Frontend fix (#22)
    * fix
    
    * fix storybook addon

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -3,9 +3,10 @@ const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
+    "@storybook/addon-actions",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-links",
   ],
   framework: "@storybook/nextjs",
   webpackFinal: async (config) => {
