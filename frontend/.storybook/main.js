5742791c19a68ca4ed9aae9726d8960187c4362c
suimenkathemove <yoshiharu2580@gmail.com>
2024-03-08 08:44:18 +0900

    refactor storybook addons

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-actions",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-links",
